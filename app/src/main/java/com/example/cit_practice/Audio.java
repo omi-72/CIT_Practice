8f3d2d71c13e3e0b1536ec7d5f458fe71c82c026
omi-72 <jannat.omi722@gmail.com>
2022-06-07 00:39:46 +0600

    Generate constructor String name & uri audioUri

--- app/src/main/java/com/example/cit_practice/Audio.java
+++ app/src/main/java/com/example/cit_practice/Audio.java
@@ -1,4 +1,14 @@
 package com.example.cit_practice;
 
+import android.net.Uri;
+
 public class Audio {
+
+    String name;
+    Uri audioUri;
+
+    public Audio(String name, Uri audioUri) {
+        this.name = name;
+        this.audioUri = audioUri;
+    }
 }
